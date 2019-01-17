24c8e9afe1c0bbcbcb0e97ba90de1fb3e1f665cd
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2019-01-17 12:17:47 -0600

    udpated README.md

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # barkorem-ipsuruff
 ### Github: @lindsayspencer 
 ### Twitter: @lindscatspencer 
+
+Add some 'ruff!' to your stuff! A lorem ipsum generator for dog-lovers. Originally made as a [text generator site](https://ecstatic-noyce-65a5fb.netlify.com/) using Gatsby.js. 
+
+## Install 
+
+`npm install barkorem-ipsuruff`
+
+## Usage
+
+Get a single paragraph (a string) of generated, puppy-fied text in small `'S'`, medium `'M'`, or large `'L'`. Default is medium if no size is specified. 
+
+```
+const bark = require("barkorem-ipsuruff");
+
+bark('S');
+bark('M');
+bark('L');
+bark();
+```
--- index.js
+++ index.js
@@ -27,8 +27,7 @@ const ipsuruff = () => {
 // The actual work bank
 const puppyWordBank = ["barkorem", "barkpsum", "barkolor", "barkit", "barkmet", "barkonsectetur", "barkdipiscing", "barklit", "barked", "barko", "barkiusmod", "barkempor", "barkncididunt", "barkt", "barkabore", "barkt", "barkolore", "barkagna", "barkliqua", "barkt", "barknim", "barkd", "barkinim", "barkeniam", "barkuis", "barkostrud", "barkxercitation", "barkllamco", "barkaboris", "barkisi", "barkt", "barkliquip", "barkx", "barka", "barkommodo", "barkonsequa", "barkuis", "barkute", "barkrure", "barkolor", "barkn", "barkeprehenderit", "barkn", "barkoluptate", "barkelit", "barksse", "barkillum", "barkolore", "barku", "barkugiat", "barkulla", "barkariatur", "barkxcepteur", "barkint", "barkccaecat", "barkupidatat", "barkon", "barkroident", "barkunt", "barkn", "barkulpa", "barkui", "barkfficia", "barkeserunt", "barkollit", "barknim", "barkd", "barkst", "barkaborum", "Loreruff", "ipsuruff", "doloruff", "siruff", "ameruff", "consecteturuff", "adipiscinruff", "eliruff", "seruff", "druff", "eiusmoruff", "temporuff", "incididunruff", "uruff", "laborruff", "eruff", "dolorruff", "mruffgna", "ruffliqua", "Uruff", "eniruff", "aruff", "ruffinim", "veniaruff", "quiruff", "nostruruff", "exercitatioruff", "ullamcruff", "laboriruff", "nruffsi", "uruff", "aliquiruff", "eruff", "eruff", "cruffmmodo", "consequruff", "Duiruff", "autruff", "irurruff", "doloruff", "iruff", "reprehenderiruff", "iruff", "voluptatruff", "veliruff", "ruffsse", "cilluruff", "dolorruff", "eruff", "fugiaruff", "nullruff", "paruffiatur", "Excepteuruff", "sinruff", "occaecaruff", "cupidaruffat", "ruffon", "proidenruff", "sunruff", "iruff", "culpruff", "quruff", "officiruff", "deserunruff", "molliruff", "aniruff", "iruff", "esruff", "laboruruff"];
 
-module.exports = {
-    barksum: (size) => {
+module.exports = function getBarksum(size){
         let bank = puppyWordBank;
         let text = "";
         let sentenceNumber;
@@ -61,4 +60,3 @@ module.exports = {
         }
 		return text;
     }
-}
